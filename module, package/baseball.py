9a56f0d820c28fde172deb8ace553405018fdf67
chaehyeon7 <81363193+chaehyeon7@users.noreply.github.com>
2021-09-13 14:06:15 +0900

    길이 에러도 발생 시키지 않고 다시 입력하게 처리

--- module, package/baseball.py
+++ module, package/baseball.py
@@ -1,4 +1,5 @@
 from baseball_game_engine import make_answer, check
+from custom_error import InvalidLenghtError
 
 answer = make_answer()
 
@@ -6,6 +7,17 @@ answer = make_answer()
 while True:
 # 숫자묻자
     guess = input("뭘까?")
+    # 숫자인지 아닌지 확인하자
+    try:
+        guess_int = int(guess)
+    except ValueError as e:
+        print('숫자를 입력하세요')
+        continue
+    if len(guess) != len(answer):   #3
+        raise InvalidLenghtError('정답의 길이와 다릅니다.')
+        print(f'정답의 길이와 다른 것을 입력했네요. {len(answer)}문자')
+        continue
+
 # strike, ball 판정하자
     strike, ball = check(guess, answer)
 # 출력하자
