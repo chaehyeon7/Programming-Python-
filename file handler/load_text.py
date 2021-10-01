558611c00c56d1d9924a1673a18aa2ace77b5e58
chaehyeon7 <81363193+chaehyeon7@users.noreply.github.com>
2021-10-01 15:38:52 +0900

    load text, ried

--- file handler/load_text.py
+++ file handler/load_text.py
@@ -6,6 +6,11 @@ f.close()
 print(data)
 
 
+with open('text.txt', 'r', encoding='utf-8') as f:
+    data = f.read()
+print(data)
+
+
 print('한줄 씩 읽기')
 f = open('text.txt', 'r', encoding='utf-8')
 while True:
--- file handler/save_text.py
+++ file handler/save_text.py
@@ -1,3 +1,4 @@
+
 f = open('text.txt', 'w', encoding='utf-8')
 
 f.write('이유빈:')
@@ -7,3 +8,10 @@ f.write('김효진:')
 f.write('하늘색')
 
 f.close()
+
+with open('text.txt', 'w', encoding='utf-8') as f:
+    f.write('이유빈:')
+    f.write('초록색')
+    f.write('\n')
+    f.write('김효진:')
+    f.write('하늘색')  # close() 필요없음 with나갈때 자동으로 close()함
